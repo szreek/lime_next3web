9708971f9436037f52d37e3635aa80d03bc6a0ce
lszweda <lszweda@psi.pl>
2022-11-09 19:47:14 +0100

    constants adjustment

--- constants/index.ts
+++ constants/index.ts
@@ -1,15 +1,13 @@
-const { GOERLI_URL, PUBLIC_KEY_LOCAL, USELECTION_CONTRACT_ADDRESS } = process.env;
-
   export interface Networks {
     [key: number]: string;
   }
   export const walletConnectSupportedNetworks: Networks = {
     // Add your network rpc URL here
-    1: GOERLI_URL
+    1: 'https://goerli.infura.io/v3/4d5caad6cbc645eba02a8cd5dc0036bb'
   };
 
   // Network chain ids
   export const supportedMetamaskNetworks = [1, 3, 4, 5, 42];
 
-  export const ALBT_TOKEN_ADDRESS = PUBLIC_KEY_LOCAL;
-  export const US_ELECTION_ADDRESS = USELECTION_CONTRACT_ADDRESS;
+  export const ALBT_TOKEN_ADDRESS = '0xb2Cb8eEB2DBcdeB0B47e3E5dB55a902C7B5C5C07';
+  export const US_ELECTION_ADDRESS = '0x30C3dBC8d796c311eAc5839f5E5C24a33c3bcb8f';
